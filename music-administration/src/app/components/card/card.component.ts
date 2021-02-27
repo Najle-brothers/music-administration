b9cc204f4ec4288231c14a3e3d7fc3e6a0e9228e
Micaela Najle <micaelanajle@gmail.com>
2021-02-27 18:01:34 -0300

    card component

--- music-administration/src/app/components/card/card.component.ts
+++ music-administration/src/app/components/card/card.component.ts
@@ -1,3 +1,4 @@
+import { AbstractEmitterVisitor } from '@angular/compiler/src/output/abstract_emitter';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,4 +13,31 @@ export class CardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public albums = [
+    {
+      artist: 'Artista 01',
+      title: 'Titulo 01',
+      picture: '../../../assets/img/headphones.png'
+    },
+    {
+      artist: 'Artista 02',
+      title: 'Titulo 02',
+      picture: '../../../assets/img/headphones.png'
+    },
+    {
+      artist: 'Artista 03',
+      title: 'Titulo 03',
+      picture: '../../../assets/img/headphones.png'
+    },
+    {
+      artist: 'Artista 04',
+      title: 'Titulo 04',
+      picture: '../../../assets/img/headphones.png'
+    },
+    {
+      artist: 'Artista 05',
+      title: 'Titulo 05',
+      picture: '../../../assets/img/headphones.png'
+    }
+  ]
 }
