3ee2333d6d0d89251f7c4c864906b22bcca0b87c
Lucas Najle <lucas.g.najle@gmail.com>
2021-07-02 15:30:03 -0300

    Add deeze proxy

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const httpProxy = require('express-http-proxy');
+const PORT = process.env.PORT || 5000;
+
+// Proxy deezer api
+const apiProxy = httpProxy('https://api.deezer.com/', {
+  proxyReqPathResolver: function (req, res) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        const uri = decodeURIComponent(req.url.split('uri=').pop().split('&token=').shift().split('&provider=').shift())
+        const url= 'https://api.deezer.com' + uri.split('/api')[1];
+        resolve(url);
+      }, 200);
+    });
+  }
+});
+app.get(`${process.env.DIR || ''}/api/*`, apiProxy);
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/music-administration'));
@@ -11,4 +27,6 @@ app.get('/*', function(req, res) {
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
